b4d4eff2d2490650f9e3ac6edf41287933c5d059
Zachary <zach.lee96@gmai.com>
2024-03-09 14:24:58 -0700

    Initial Commit

---
+++ Lab5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab5
+//
+//  Created by Zachary Lee on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab5/Lab5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab5App.swift
+//  Lab5
+//
+//  Created by Zachary Lee on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
